c33d76eb8194c10c61a17d47b53e677f4183a5a2
Gazaunga <Gazaunga@gmail.com>
2017-06-28 16:35:37 -0400

    Create script.js

---
+++ Highlight Link Targer/script.js
@@ -0,0 +1,21 @@
+var link = document.querySelectorAll('a');
+var highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+  var linkCoords = this.getBoundingClientRect();
+ 
+  var coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+link.forEach(a => a.addEventListener('mouseenter', highlightLink));
