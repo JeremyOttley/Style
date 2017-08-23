36b596e9477024f3cf91825522da27ccc110fad6
Gazaunga <Gazaunga@gmail.com>
2017-08-23 20:29:12 +0000

    Add files via upload

---
+++ forest/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Let's define the required varibles such as the number of particles to be created and an array to hold the particles
+var particleContainer = document.getElementById('particles');
+var Nparticles = 100;
+var particles = [];
+
+// function to return a random number from a given min and max
+function rand(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// This constructor will generate the required information for each particle and it's html structure to be added to the document
+function createParticle(i) {
+	this.id = i;
+	this.width = rand(1, 20) + 'px';
+	this.height = this.width;
+	this.x = rand(10, 90) + '%';
+	this.delay = rand(1, 60) + 's';
+	this.duration = rand(10, 60) + 's';
+	this.html = '<span style=" width: ' + this.width + '; height: ' + this.height + '; left: ' + this.x + '; animation-delay: ' + this.duration + '; animation-duration: ' + this.duration + '; "></span>';
+}
+
+// Let's loop through till we reach the max number of particles and save them to the array and append them to the document
+while (particles.length <= Nparticles) {if (window.CP.shouldStopExecution(1)){break;}
+	var Particle = new createParticle(particles.length);
+	particles.push(Particle);
+	particleContainer.innerHTML += Particle.html;
+}
+window.CP.exitedLoop(1);
+
+
+// Hope you liked it and that has inspired you to create something awesome
